69622d0c141bbc08b56a661a8ff066369e9fb4aa
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-02-14 09:22:11 -0500

    refact the code

--- src/App.js
+++ src/App.js
@@ -1,83 +1,51 @@
 import React, { useState } from 'react';
 import './App.css';
-
-
-
+import { evaluate } from 'mathjs'
 
 const App =()=> {
- 
   let [result, setResult] = useState('0');
- 
-
   const handleClick = (e) => {
-   
     console.log(e.target.id)
-    //setResult(e.target.name); //necesary import React, { useState } from 'react'; for use concat!
-    //result = result.slice(1, result.length)
-    //setResult(result.concat(e.target.name))
     if(result === '0' ){
       setResult("");
       setResult(prevResult => prevResult + e.target.name)
-      console.log(result); 
     } else {
       setResult(prevResult => prevResult + e.target.name)
     }
- 
-  
   }
   const calculate =() => {
     const hasOperators = /[+\-*/]/.test(result);
     if(hasOperators){
-      console.log('is process')
-      setResult(eval(result).toString());
+      setResult(evaluate(result));
     }else{
-      console.log('it dosnt containe the operators ')
       setResult('0');
     }
-    
   }
   const clear =() => {
-    setResult('0');
+    setResult(0);
   }
   return (
    <div className="calculator">
     <div id="display">{result}</div>
-   
     <button id="clear" type="button" onClick={clear}>AC</button>
     <button id="divide" name="/" className="operations" onClick={handleClick}>/</button>
     <button id="multiply" name="*" className="operations" onClick={handleClick}>X</button>
-
     <button id="seven" name="7" onClick={handleClick}>7</button>
     <button id="eight" name="8" onClick={handleClick}>8</button>
     <button id="nine" name="9" onClick={handleClick}>9</button>
     <button id="subtract" name="-" className="operations" onClick={handleClick}>-</button>
-
     <button id="four" name="4" onClick={handleClick}>4</button>
     <button id="five" name="5" onClick={handleClick}>5</button>
     <button id="six" name="6" onClick={handleClick}>6</button>
     <button id="add" name="+" className="operations" onClick={handleClick}>+</button>
-    
     <button id="one" name="1" onClick={handleClick}>1</button>
     <button id="two" name="2" onClick={handleClick}>2</button>
     <button id="three" name="3" onClick={handleClick}>3</button>
     <button id="equals" type="button" onClick={calculate}>=</button> 
-    
-
-    
-    
-    
-
-
-
     <button id="zero" name="0" onClick={handleClick}>0</button>
-    
     <button id="decimal"name="." onClick={handleClick}>.</button>
-
- 
-
    </div>
   );
 }
-
 
 export default App;
